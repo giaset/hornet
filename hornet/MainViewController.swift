f93c8856803c2ff3e13c696cf52d3872cc340930
Gianni Settino <g@miltonandparc.com>
2016-11-20 17:27:52 -0500

    Fuck storyboards

---
+++ hornet/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  hornet
+//
+//  Created by Gianni Settino on 2016-11-20.
+//  Copyright © 2016 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+}
